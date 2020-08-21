7b0bee2d92e8447e1b056f63ae6fe71a86a69013
tchiffaMed <mahamadou.tchiffa@codeloccol.org>
2020-08-21 16:54:08 +0100

    update

---
+++ agogo/agogo.js
@@ -0,0 +1,21 @@
+let countdown;
+
+function timer(seconds) {
+    const now = Date.now();
+    const then = now + seconds * 1000;
+
+    countdown = setInterval(() =>{
+        const secondsLeft = Math.round((then - Date.now())/1000);
+
+        if(secondsLeft >= 0) {
+            clearInterval(countdown);
+            return;
+        }
+        console.log(secondsLeft);
+    }, 1000);
+}
+
+
+function displayTimeLeft(seconds) {
+    console.log(seconds);
+}
