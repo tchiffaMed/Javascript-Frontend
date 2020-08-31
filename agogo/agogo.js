8007b7f0d7de3430e1d3138059a2ac9d6ae20a17
ecrits <lecture.cheikh@gmail.com>
2020-08-31 07:56:57 +0100

    update

--- agogo/agogo.js
+++ agogo/agogo.js
@@ -1,26 +1,61 @@
 let countdown;
 const timerDisplay = document.querySelector('.display__time-left');
+const endTime = document.querySelector('.display__end-time');
+const buttons = document.querySelectorAll('[data-time]');
 
 function timer(seconds) {
-    const now = Date.now();
-    const then = now + seconds * 1000;
+    //clear any existingtimers
+    clearInterval(countdown);
 
-    countdown = setInterval(() =>{
-        const secondsLeft = Math.round((then - Date.now())/1000);
 
-        if(secondsLeft >= 0) {
-            clearInterval(countdown);
-            return;
-        }
-        console.log(secondsLeft);
+   const now = Date.now();
+   const then = now + seconds * 1000;
+   displayTimeLeft(seconds);
+   displayEndTime(then);
+
+
+  countdown = setInterval(() => {
+        const secondsLeft = Math.round((then - Date.now()) / 1000);
+        // creer un arret
+         if(secondsLeft < 0) {
+             clearInterval(countdown);
+             return;
+         }
+         //affichage
+        displayTimeLeft(secondsLeft);
     }, 1000);
 }
 
 
 function displayTimeLeft(seconds) {
-    console.log(seconds);
     const minutes = Math.floor(seconds / 60);
     const remainderSeconds = seconds % 60;
-    const display = `${minutes} : ${remainderSeconds}`;
+    const display = `${minutes}:${remainderSeconds < 10 ? '0' : '' }${remainderSeconds}`;
+    document.title = display;
     timerDisplay.textContent = display;
+
 }
+
+function displayEndTime(timestamp) {
+    const end = new Date(timestamp);
+    const hour = end.getHours();
+    const adjustedHour = hour > 12 ? hour - 12 : hour;
+    const minutes = end.getMinutes();
+    let returnBack = `${adjustedHour}:${minutes < 10 ? '0' : ''}${minutes}`;
+    let returnBackColor = returnBack.fontcolor('blue')
+    endTime.textContent = `Be Back At `  +  returnBack;
+}
+
+function startTimer() {
+    const seconds = parseInt(this.dataset.time);
+    timer(seconds);
+}
+
+buttons.forEach(button => button.addEventListener('click', startTimer));
+document.customForm.addEventListener('submit', function(e) {
+    e.preventDefault();
+    const mins = this.minutes.value;
+    console.log(mins);
+    timer(mins * 60);
+    this.reset();
+});
